f996d32b2deb17d9711d9cfe3c90e3f86c923af6
Lynda Gunnarsdottir <lg223fn@student.lnu.segit config --global user.emailgit config --global user.email lg223fn@student.lnu.segit config --global user.email.cler.clear$git config --global user.email lg223fn@student.lnu.se>
2025-09-04 18:27:29 +0200

    Add affirmation cat class

--- src/affirmationCat.js
+++ src/affirmationCat.js
@@ -1,6 +1,15 @@
 
+import readline from 'readline'
+
 
 export class AffirmationCat {
+
+  constructor() {
+    this.readLine = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout
+    })
+  }
 
   async fetchAffirmation() {
 
@@ -10,8 +19,54 @@ export class AffirmationCat {
       return data.affirmation
 
     } catch {
-    console.error("Error fetching affirmation", data)
+      console.error("Error fetching affirmation", data)
     }
   
+  }
+
+  getCatAscii() {
+    return `  
+              |\\_._/|        
+              | o o |        
+              (  T  )       
+             .^\`-^-"'^.     
+             \`.  ;  .'     
+             | | | | |     
+            ((_((|))_)) `
+  }
+
+  getComputerCatAscii() {
+    return  `
+                         \\  / 
+                          \\/         
+            ___             
+       _.-|   |          |\\__/,|   (\`\\
+      {   |   |          |o o  |__ _) )
+       "-.|___|        _.( T   )  \`  /
+         .--'-\\-.     _((_ \\\`^--' /_<  \\
+        .+|______|__.-||__)\\'-'(((/  (((/`
+  }
+
+  displayWelcomeMessage() {
+    console.log(this.getCatAscii())
+    console.log("    \n\n Affirmation cat wants to tell you something. \n")
+  }
+
+  displayAffirmation(name, affirmation) {
+    console.log(`\n\n\n\n\n Nice to meet you, ${name}!  \n\n\n     ${affirmation}. Take care and happy coding ♡`)
+    console.log(this.getComputerCatAscii())
+    this.readLine.close()
+  }
+
+   promptName(affirmation) {
+      this.readLine.question("\n        Enter your name: ", (name) => {
+      this.displayAffirmation(name, affirmation)
+    })
+  }
+
+  async runAffirmationCat() {
+   const affirmation = await this.fetchAffirmation()
+   this.displayWelcomeMessage()
+   this.promptName(affirmation)
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 
 import { AffirmationCat } from "./affirmationCat.js"
 
+
 const affirmationCat = new AffirmationCat() 
 
-const affirmation = await affirmationCat.fetchAffirmation()
-
-console.log(affirmation)
+affirmationCat.runAffirmationCat()
